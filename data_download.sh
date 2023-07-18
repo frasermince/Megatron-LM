cd748ee764580778f09785c1ffc95a82388644b8
Fraser Mince <frasermince@gmail.com>
2023-07-18 13:38:30 -0600

    Fix data download.

--- data_download.sh
+++ data_download.sh
@@ -1,5 +1,5 @@
 mkdir data
-cd data
 pip install wikiextractor
+cd data
 wget https://dumps.wikimedia.org/enwiki/latest/enwiki-latest-pages-articles.xml.bz2
 python -m wikiextractor.WikiExtractor enwiki-latest-pages-articles.xml.bz2 --json
