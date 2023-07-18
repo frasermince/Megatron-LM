02a18d90869c631084bddca16d0f408f46d82f13
Fraser Mince <frasermince@gmail.com>
2023-07-18 08:33:00 -0600

    Add url for data script.

--- data_download.sh
+++ data_download.sh
@@ -2,4 +2,4 @@ mkdir data
 cd data
 pip install wikiextractor
 wget https://dumps.wikimedia.org/enwiki/latest/enwiki-latest-pages-articles.xml.bz2
-python -m wikiextractor.WikiExtractor wikidataset --json
+python -m wikiextractor.WikiExtractor enwiki-latest-pages-articles.xml.bz2 --json
