89ac9cfd5a00a613469200b4372b8d7ddd39f27f
Fraser Mince <frasermince@gmail.com>
2023-07-18 09:19:03 -0600

    Remove clone.

--- gpu-startup.sh
+++ gpu-startup.sh
@@ -1,4 +1,3 @@
-cd ~ && git clone https://github.com/NVIDIA/Megatron-LM.git
 cd ~/Megatron-LM && pip install -e .
 cd ~/Megatron-LM && mkdir bert
 cd ~/Megatron-LM/bert && wget --content-disposition https://api.ngc.nvidia.com/v2/models/nvidia/megatron_bert_345m/versions/v0.1_uncased/zip -O megatron_bert_345m_v0.1_uncased.zip
