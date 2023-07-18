2cedcb64de504b736719f010ef31f0f73f810748
Fraser Mince <frasermince@gmail.com>
2023-07-18 08:54:28 -0600

    Add home directory.

--- gpu-startup.sh
+++ gpu-startup.sh
@@ -1,7 +1,6 @@
 cd ~ && git clone https://github.com/NVIDIA/Megatron-LM.git
-cd Megatron-LM && pip install -e .
-cd Megatron-LM && mkdir bert
-cd bert
-cd Megatron-LM/bert && wget --content-disposition https://api.ngc.nvidia.com/v2/models/nvidia/megatron_bert_345m/versions/v0.1_uncased/zip -O megatron_bert_345m_v0.1_uncased.zip
-cd Megatron-LM/bert && wget https://s3.amazonaws.com/models.huggingface.co/bert/bert-large-uncased-vocab.txt
+cd ~/Megatron-LM && pip install -e .
+cd ~/Megatron-LM && mkdir bert
+cd ~/Megatron-LM/bert && wget --content-disposition https://api.ngc.nvidia.com/v2/models/nvidia/megatron_bert_345m/versions/v0.1_uncased/zip -O megatron_bert_345m_v0.1_uncased.zip
+cd ~/Megatron-LM/bert && wget https://s3.amazonaws.com/models.huggingface.co/bert/bert-large-uncased-vocab.txt
 
